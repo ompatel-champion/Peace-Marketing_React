0da2e0ccac1262e5d18fe5f8ae16ed8b61b33a14
ompatel-champion <ompatelupwork@gmail.com>
2022-04-22 08:46:26 +0800

    add craco color shceme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.less';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Component } from 'react';
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import './App.less';
 import App from './App';
-import 'antd/dist/antd.css';
+// import 'antd/dist/antd.css';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
